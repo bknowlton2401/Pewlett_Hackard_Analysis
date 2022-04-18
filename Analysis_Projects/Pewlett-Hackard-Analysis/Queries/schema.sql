4a4fc76980079f9a9daab810504ef3a671854e50
Bethany Knowlton <bknowlton22@gmail.com>
2022-04-17 18:03:30 -0700

    update schema

--- Analysis_Projects/Pewlett-Hackard-Analysis/Queries/schema.sql
+++ Analysis_Projects/Pewlett-Hackard-Analysis/Queries/schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE employees (
 
 --create table for deptartment managers
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -30,13 +30,32 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 
 --create table for salaries
 CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+ 	from_date DATE NOT NULL,
+ 	to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
 
+--create table for dept_emp
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE not null,
+	to_date DATE not null,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no)
+);
+
+--create a table for titles
+CREATE TABLE titles(
+	emp_no int not null,
+	title varchar (40) not null,
+	from_date date not null,
+	to_date date not null,
+  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+ 	PRIMARY KEY (emp_no)
+);
 --run a query
 select * from departments;
